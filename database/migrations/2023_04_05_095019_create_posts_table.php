ca84ab2a18e40c0fc0037fc1af9ff1ff7ee61ec3
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-05 15:45:06 +0500

    Post migration created

--- database/migrations/2023_04_05_095019_create_posts_table.php
+++ database/migrations/2023_04_05_095019_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }
