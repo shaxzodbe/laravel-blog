7e066a94cc272fc008254283c98818ed4058bed4
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-06 20:11:50 +0500

    Mutators added to Category model created_at, updated_at field

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use function Pest\Laravel\get;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'created_at'];
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('d-m-Y');
+    }
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('d-m-Y');
+    }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -23,6 +23,12 @@
                                 Title
                             </th>
                             <th scope="col" class="px-6 py-3">
+                                Created at
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Updated at
+                            </th>
+                            <th scope="col" class="px-6 py-3">
                                 Actions
                             </th>
                         </tr>
@@ -36,6 +42,12 @@
                                 </th>
                                 <td class="px-6 py-4">
                                     {{ $category->title }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $category->created_at }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $category->updated_at }}
                                 </td>
                                 <td class="px-6 py-4">
                                     <a class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
