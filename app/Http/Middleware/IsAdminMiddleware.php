89ae6b97cc71f8ef966b72058f7b53561ffeae39
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-06 14:54:16 +0500

    IsAdminMiddleware corrected

--- app/Http/Middleware/IsAdminMiddleware.php
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -16,7 +16,7 @@ class IsAdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth::user()->is_admin) {
+        if (!auth::user()->is_admin) {
             abort(403);
         }
         return $next($request);
