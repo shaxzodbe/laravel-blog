99c96d5922b60ed32ea5e0af0380406eaa2331bd
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-04-28 19:54:01 +0500

    Add Attribute to get difference between created time user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use function Pest\Laravel\get;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function createdAtDiff(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->created_at->diffForHumans()
+        );
+    }
 }
